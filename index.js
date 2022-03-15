73514b1d35c2dea74f16e32d581f9dbd36038161
Roselynn <worklaptop@Roselynns-MacBook-Pro.local>
2022-03-15 12:01:20 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// write your code here
+//create a server
+//http enables you to make request to servers
+const http = require('http');
+//server that listens on port 8080
+const port = 8080;
+
+//server object
+const server = http.createServer(function(req,res){
+  console.log('Received' + req.method + 'request for:' + req.url);
+
+  //1.send headers 
+  res.writeHead(200, {'Content-Type':'text/plain'});
+
+  //2.send body response When the Port is accessed, it responds with Hello World
+  res.end('Hello World');
+})
+//Add a listener so that your server will have a way to handle incoming connections.
+//Listen for incoming connections.
+server.listen(8080, 'localhost', null, function (){
+// log a message that the server is listening and port
+console.log('Server is listening on localhost:8080');
+
+});
